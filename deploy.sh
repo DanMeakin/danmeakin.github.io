7348e36e437d1c3d788bc14fc07dddf30ce31cb7
Dan Meakin <dan@meak.in>
2020-06-01 13:50:46 +0200

    Changes to setup and pages

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+# From https://jaspervdj.be/hakyll/tutorials/github-pages-tutorial.html
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack exec meakdotin clean
+stack exec meakdotin build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+rsync -a --filter='P _site/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         _site/ .
+
+# Commit
+git add -A
+git commit -m "Publish"
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
